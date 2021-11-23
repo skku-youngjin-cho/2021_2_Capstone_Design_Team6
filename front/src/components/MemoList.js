508a65b0ab6a4f85d83d09a595a710676cb1c63f
YJY2690 <dusk2690@g.skku.edu>
2021-11-23 21:24:29 +0900

    스타일 수정 및 임시적인 전송영역 추가

--- front/src/components/MemoList.js
+++ front/src/components/MemoList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import MemoItem from './MemoItem';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
+import * as RiIcons from "react-icons/ri";
 
 const MemoListBlock = styled.div`
 display: flex;
@@ -22,8 +23,10 @@ function MemoList( props ) {
                 return(
                     <div
                         style={{
-                            display: "flex",
                             flexDirection: "column",
+                            width: "auto",
+                            display: "flex",
+                            flexWrap: "wrap",
                             alignItems: "center",
                         }}
                         key={areaId}
@@ -37,12 +40,9 @@ function MemoList( props ) {
                                             {...provided.droppableProps}
                                             ref={provided.innerRef}
                                             style={{
-                                            background: snapshot.isDraggingOver
-                                                ? "lightblue"
-                                                : "lightgrey",
-                                            padding: 4,
-                                            display: "flex",
-                                            "flex-direction" : "column"
+                                                padding: 4,
+                                                display: "flex",
+                                                width: "auto"
                                             }}
                                         >
                                             {Object.values(area).map((item, index) => {
@@ -81,6 +81,44 @@ function MemoList( props ) {
                     </div>
                 );
             })}
+
+<Droppable droppableId="Droppable" key="Droppable">
+            {(provided, snapshot) => {
+                                    return (
+                                        <div
+                                            {...provided.droppableProps}
+                                            ref={provided.innerRef}
+                                            style={{
+                                                background: "#5483b1",
+                                                zIndex: 5,
+                                                width: snapshot.isDraggingOver
+                                                    ? 80
+                                                    : 30,
+                                                height: snapshot.isDraggingOver
+                                                    ? 80
+                                                    : 30,
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "center",
+                                                fontSize: 15,
+                                                position: "fixed",
+                                                left: "80%",
+                                                bottom: 40,
+                                                transform: "translate(-50%, 50%)",
+                                                color: "white",
+                                                borderRadius: "50%",
+                                                border: "none",
+                                                outline: "none",
+                                                transition: "0.125s all ease-in"
+                                            }}
+                                        >
+                                            <RiIcons.RiMailSendFill />
+                                            {provided.placeholder}
+                                        </div>
+                                        
+                                    );
+                }}
+            </Droppable>
         </MemoListBlock>
   );
 }
